54864a73eb8256e009fbace8441333a1390c3002
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-05-07 00:23:20 +0530

    Tight coupling example by changing the game

--- learn-spring-framework/src/main/java/com/app/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/app/LearnSpringFrameworkApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.app.game.GameRunner;
 import com.app.game.MarioGame;
+import com.app.game.SuperContraGame;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
 	//	SpringApplication.run(LearnSpringFrameworkApplication.class, args);
-		MarioGame game = new MarioGame();
+//		MarioGame game = new MarioGame();
+		SuperContraGame game = new SuperContraGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
 	}
